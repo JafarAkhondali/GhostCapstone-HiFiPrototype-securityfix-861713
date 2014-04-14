9eec23d83b2d9627d14eeb5431824fd4d3682311
Ted Tagami <tedtag@uw.edu>
2014-04-14 12:09:50 -0700

    started cleaning up gestures and implementing robbies code

--- mainPrototype/leapscript.js
+++ mainPrototype/leapscript.js
@@ -49,161 +49,16 @@ function leapToScene(leapPos) {
     return [x, -y];
 }
 
-function onCircle(gesture) {
-
-    /*
-      
-        Setting up our parameters
-      
-      */
-
-    // First get the position using our leapToScene function
-    var pos = leapToScene(gesture.center);
-
-    // Assigning the radius
-    var radius = gesture.radius;
-
-    var clockwise = false;
-
-    if (gesture.normal[2] <= 0) {
-
-        clockwise = true;
-
-    }
-
-    /*
-      
-        Setting up our drawing style
-
-      */
-
-    // Setting up the style for the stroke, and fill
-    c.fillStyle = "#39AECF";
-    c.strokeStyle = "#39AECF";
-    c.lineWidth = 5;
-
-
-    // Creating the path for the finger circle
-    c.beginPath();
-
-    // Draw a full circle of radius 6 at the finger position
-    c.arc(pos[0], pos[1], radius, 0, Math.PI * 2);
-
-    c.closePath();
-
-    if (clockwise)
-        c.stroke();
-    else
-        c.fill();
-
-}
-
-
-function onSwipe(gesture) {
-
-    var startPos = leapToScene(gesture.startPosition);
-    var pos = leapToScene(gesture.position);
-
-    // Rotate object on swipe if only one finger is detected
-    if (numberFingers == 1) {
-        dae.rotation.y += (pos[0] - startPos[0]) / 6000;
-    }
-
-    // Old Code
-    // camera.position.x += (pos[0] - startPos[0])/1500;
-    // camera.position.y -= (pos[1] - startPos[1])/1500;
-
-    // Setting up the style for the stroke
-    c.strokeStyle = "#FFA040";
-    c.lineWidth = 3;
-
-    // Drawing the path
-    c.beginPath();
-
-    // Move to the start position
-    c.moveTo(startPos[0], startPos[1]);
-
-    // Draw a line to current position
-    c.lineTo(pos[0], pos[1]);
-
-    c.closePath();
-    c.stroke();
-
-
-}
-
-
-function onKeyTap(gesture) {
-
-    var pos = leapToScene(gesture.position);
-
-    var time = frame.timestamp;
-
-    keyTaps.push([pos[0], pos[1], time]);
-
-}
-
-function updateKeyTaps() {
-
-    for (var i = 0; i < keyTaps.length; i++) {
-
-        var keyTap = keyTaps[i];
-        var age = frame.timestamp - keyTaps[i][2];
-        age /= 1000000;
-
-        if (age >= KEYTAP_LIFETIME) {
-            keyTaps.splice(i, 1);
+// Mapping function for Rotation gesture
+function map(value, inputMin, inputMax, outputMin, outputMax){
+        outVal = ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);  
+        if(outVal >  outputMax){
+          outVal = outputMax;
         }
-
-    }
-
-}
-
-
-function drawKeyTaps() {
-
-    for (var i = 0; i < keyTaps.length; i++) {
-
-        var keyTap = keyTaps[i];
-
-        var x = keyTap[0];
-        var y = keyTap[1];
-
-        var age = frame.timestamp - keyTap[2];
-        age /= 1000000;
-
-        var completion = age / KEYTAP_LIFETIME;
-        var timeLeft = 1 - completion;
-
-
-        /*
-        
-          Static Ring
-
-        */
-        c.strokeStyle = "#FF2300";
-        c.lineWidth = 3;
-
-        c.beginPath();
-        c.arc(x, y, KEYTAP_START_SIZE, 0, Math.PI * 2);
-        c.closePath();
-        c.stroke();
-
-
-
-        var opacity = timeLeft;
-        var radius = KEYTAP_START_SIZE * timeLeft;
-        //console.log( opacity );
-
-        c.fillStyle = "rgba( 256 , 33 , 0 , " + opacity + ")";
-
-        // Creating the path for the finger circle
-        c.beginPath();
-        console.log(x + " " + y);
-        c.arc(x, y, radius, 0, Math.PI * 2);
-        c.closePath();
-        c.fill();
-    }
+        if(outVal <  outputMin){
+          outVal = outputMin;
+        } 
+        return outVal;
 }
 
 function onScreenTap(gesture) {
@@ -316,6 +171,7 @@ controller.on('frame', function (data) {
 
     // Clears the window
     c.clearRect(0, 0, width, height);
+
     // Loops through each hand
     for (var i = 0; i < frame.hands.length; i++) {
 
@@ -323,7 +179,9 @@ controller.on('frame', function (data) {
         var hand = frame.hands[i]; // The current hand
         var handPos = leapToScene(hand.palmPosition); // Palm position
         var scaleFactor = hand.scaleFactor(lastFrame, frame);
+        var translation = lastFrame.translation(frame);
 
+        /* GESTURES */
         // ZOOM GESTURE - Pinch Motion
         if (numberFingers == 2 & scaleFactor < 1) { // Zoom out
             camera.position.z += (1 - scaleFactor) * 2;
@@ -332,22 +190,18 @@ controller.on('frame', function (data) {
             camera.position.z -= (scaleFactor - 1) * 2;
             //camera.position.x -= (scaleFactor - 1) * 2;
         }
-        // Loops through each finger
 
+        
+
+        dae.rotation.x = map(translation[1], -4, 4, 0, 10);
+        dae.rotation.y = map(translation[0], -4, 4, 0, 10);
+
+        // Loops through each finger
         for (var j = 0; j < hand.fingers.length; j++) {
             var finger = hand.fingers[j]; // Current finger
             var fingerPos = leapToScene(finger.tipPosition); // Finger position
 
-            /*            // 1. Connect finger to hand
-            c.strokeStyle = "#FFA040";
-            c.lineWidth = 3;
-            c.beginPath();
-              c.moveTo(handPos[0], handPos[1]);
-              c.lineTo(fingerPos[0], fingerPos[1]);
-            c.closePath();
-            c.stroke();
-*/
-            // 2. Drawing the finger
+            // Drawing the finger
             c.strokeStyle = "#FF5A40";
             c.lineWidth = 6;
             c.beginPath();
@@ -356,14 +210,6 @@ controller.on('frame', function (data) {
             c.stroke();
 
         }
-
-        /*          // 3. Drawing the palm
-          c.fillStyle = "#FF5A40";
-          c.beginPath();
-            c.arc(handPos[0], handPos[1], 10, 0, Math.PI*2);
-          c.closePath();
-          c.fill();
-*/
     }
 
     /* Gestures */
@@ -375,14 +221,6 @@ controller.on('frame', function (data) {
 
         switch (type) {
 
-        case "circle":
-            onCircle(gesture);
-            break;
-
-        case "swipe":
-            onSwipe(gesture);
-            break;
-
         case "screenTap":
             onScreenTap(gesture);
             break;
@@ -391,9 +229,6 @@ controller.on('frame', function (data) {
         }
 
     }
-
-    updateKeyTaps();
-    drawKeyTaps();
 
     updateScreenTaps();
     drawScreenTaps();
